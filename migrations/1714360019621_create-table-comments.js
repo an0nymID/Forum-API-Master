9486581c9cdb9d9507e2f1af95fde8b654a458bc
an0nymID <m.ihsan2131@gmail.com>
2024-05-15 10:37:56 +0800

    continues project init forum api

---
+++ migrations/1714360019621_create-table-comments.js
@@ -0,0 +1,51 @@
+exports.up = (pgm) => {
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    is_delete: {
+      type: 'BOOLEAN',
+      notNull: true,
+      default: false,
+    },
+    like_count: {
+      type: 'INT',
+      notNull: true,
+      default: 0,
+    },
+    thread_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint(
+    'comments',
+    'fk_comments.thread_id_threads.id',
+    'FOREIGN KEY(thread_id) REFERENCES threads(id) ON DELETE CASCADE',
+  );
+  pgm.addConstraint(
+    'comments',
+    'fk_comments.owner_users.id',
+    'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE',
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('comments', 'fk_comments.thread_id_threads.id');
+  pgm.dropConstraint('comments', 'fk_comments.owner_users.id');
+  pgm.dropTable('comments');
+};
---
+++ src/Domains/comments/CommentRepository.js
@@ -0,0 +1,31 @@
+class CommentRepository {
+  async addComment(postComment) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyCommentAvailability(id, threadId) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyCommentOwner(id, owner) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getCommentsByThreadId(threadId) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteCommentById(id) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async reduceCommentLike(id) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async addCommentLike(id) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = CommentRepository;
